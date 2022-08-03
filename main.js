7e389a748fb9546f07c1a7cd697b52ef299f9a48
Mark <mgalex12@icloud.com>
2022-08-03 16:51:38 -0400

    Completed Assignment

--- main.js
+++ main.js
@@ -3,8 +3,43 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let heart = document.querySelector(".like-glyph")
+let errorModal = document.querySelector("#modal")
+//Add "click" event listener to heart
+heart.addEventListener("click", () => {
+  //receiving promise from server
+  let serverPromise = mimicServerCall()
 
+  //Handling server promise success
+  serverPromise.then((value) => {
+    
+    if(heart.textContent == FULL_HEART){
+      heart.classList.remove("activated-heart")
+      heart.textContent = EMPTY_HEART 
+      console.log(value)
+    } else {
+      heart.textContent = FULL_HEART
+      heart.classList.add("activated-heart")
+    }
+    
+  //Handling server promise error
+  }).catch( (error) => {
+    // Show errorModal by removing "hidden" class
+    errorModal.classList.remove("hidden")
+   //Selecting error modal element
+   let message = errorModal.querySelector("#modal-message")
+   // Assign promise error message to error modal
+   message.textContent = error
+    console.log(error)
 
+    //In 3 seconds, hiding error message by adding "hidden" class
+    setTimeout(() => {
+      errorModal.classList.add("hidden")
+    }, 3000)
+    
+  })
+
+})
 
 
 //------------------------------------------------------------------------------
